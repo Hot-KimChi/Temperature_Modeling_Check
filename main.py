391ab3c532e322b9859d8efc32ce9430f9fa3d6d
InSeop Hwang <inseop.hwang@hotmail.com>
2023-09-07 22:08:45 +0900

    update sql update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-
+#
 def func_sql_get(server_address, ID, password, list_databases, export_database=None, query=None):
     try:
         if case == 'model_fit':
